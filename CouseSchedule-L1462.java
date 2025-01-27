9af6d9cbeb745b4ae50cda7d2f01eb36de91cfb4
Navtej968 <108707416+Navtej968@users.noreply.github.com>
2025-01-27 08:33:51 +0530

    Create CouseSchedule-L1462.java

---
+++ CouseSchedule-L1462.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        boolean[][] reachable = new boolean[numCourses][numCourses];
+        for (int[] prereq : prerequisites) {
+            reachable[prereq[0]][prereq[1]] = true;
+        }
+        for (int k = 0; k < numCourses; k++) {
+            for (int i = 0; i < numCourses; i++) {
+                for (int j = 0; j < numCourses; j++) {
+                    if (reachable[i][k] && reachable[k][j]) {
+                        reachable[i][j] = true;
+                    }
+                }
+            }
+        }
+        List<Boolean> result = new ArrayList<>();
+        for (int[] query : queries) {
+            result.add(reachable[query[0]][query[1]]);
+        }
+
+        return result;
+    }
+}
