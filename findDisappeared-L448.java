21327e0ce46ee48be421534e007b449ae7f9f06a
Navtej968 <108707416+Navtej968@users.noreply.github.com>
2025-02-15 09:36:37 +0530

    Create findDisappeared-L448.java

---
+++ findDisappeared-L448.java
@@ -0,0 +1,18 @@
+for (int i : nums) {
+            int index = Math.abs(i);
+            if (nums[index - 1] > 0) {
+                nums[index - 1] *= -1;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+
+
+//time o(n)
+//space o(1)
+//sort of inplace hashing
