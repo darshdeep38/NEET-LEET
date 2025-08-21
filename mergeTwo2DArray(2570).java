9cfff7ff380a7973e1dce9e380bdc1947441145e
Navtej968 <108707416+Navtej968@users.noreply.github.com>
2025-08-21 15:53:13 +0530

    Create mergeTwo2DArray(2570).java

---
+++ mergeTwo2DArray(2570).java
@@ -0,0 +1,47 @@
+// (* You are given two 2D integer arrays nums1 and nums2.
+
+// nums1[i] = [idi, vali] indicate that the number with the id idi has a value equal to vali.
+// nums2[i] = [idi, vali] indicate that the number with the id idi has a value equal to vali.
+// Each array contains unique ids and is sorted in ascending order by id.
+
+// Merge the two arrays into one array that is sorted in ascending order by id, respecting the following conditions:
+
+// Only ids that appear in at least one of the two arrays should be included in the resulting array.
+// Each id should be included only once and its value should be the sum of the values of this id in the two arrays. If the id does not exist in one of the two arrays, then assume its value in that array to be 0.
+// Return the resulting array. The returned array must be sorted in ascending order by id.
+
+//  *)
+class Solution {
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int i = 0, j = 0;
+        List<int[]> result = new ArrayList<>();
+
+        while(i < m && j < n){
+            if(nums1[i][0] == nums2[j][0]){
+                result.add(new int[]{nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++; j++;
+            }
+            else if(nums1[i][0] < nums2[j][0]){
+                result.add(nums1[i]);
+                i++;
+            }
+            else{
+                result.add(nums2[j]);
+                j++;
+            }
+        }
+        while(i < m){
+            result.add(nums1[i]);
+            i++;
+        }
+        while(j < n){
+            result.add(nums2[j]);
+            j++;
+        }
+        return result.toArray(new int[result.size()][]); //i hate this part of java
+    }
+}
+
+
+//basically mergesort-merging 
