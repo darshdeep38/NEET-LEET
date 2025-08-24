8795f5363306108daea00256dc53d22d6841c697
Navtej968 <108707416+Navtej968@users.noreply.github.com>
2025-08-24 20:57:19 +0530

    Create SubarrayOf1(L-1493).java

---
+++ SubarrayOf1(L-1493).java
@@ -0,0 +1,53 @@
+// Given a binary array nums, you should delete one element from it.
+// Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+import java.util.*;
+
+class Solution {
+    public int longestSubarray(int[] nums) {
+        Map<Integer, Integer> left = new HashMap<>();
+        Map<Integer, Integer> right = new HashMap<>();
+        int cnt = 0, ones = 0;
+
+        // Calculate left consecutive ones before each 0
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                cnt++;
+                ones++;
+            } else {
+                left.put(i, cnt);
+                cnt = 0;
+            }
+        }
+
+        // Edge cases
+        if (ones == 0) return 0; // No 1s
+        if (ones == nums.length) return nums.length - 1; // All 1s
+
+        cnt = 0;
+
+        // Calculate right consecutive ones after each 0
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == 1) {
+                cnt++;
+            } else {
+                right.put(i, cnt);
+                cnt = 0;
+            }
+        }
+
+        // Calculate max combined length
+        int maxi = cnt;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                int leftCount = left.getOrDefault(i, 0);
+                int rightCount = right.getOrDefault(i, 0);
+                maxi = Math.max(maxi, leftCount + rightCount);
+            }
+        }
+
+        return maxi;
+    }
+}
+//hashmap left and right counter
+//can be done by sliding window
