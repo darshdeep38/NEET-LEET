bdf4d5c5d52da8e32d3c68b492dfa404376cc27c
Darshdeep Singh <115945757+darshdeep38@users.noreply.github.com>
2025-09-05 15:39:54 +0530

    Update L735-asteroidCollision.java
    Also contains C++ code for the same

--- L735-asteroidCollision.java
+++ L735-asteroidCollision.java
@@ -33,3 +33,37 @@ class Solution {
 }
 
 //just simulate
+//Here is the  C++ code :-
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> stk;
+
+        for (int i : asteroids) {
+            while (i < 0 && !stk.empty() && stk.top() > 0) {
+                int difference = stk.top() + i;
+                if (difference > 0) {
+                    i = 0;
+                } else if (difference < 0) {
+                    stk.pop();
+                } else {
+                    stk.pop();
+                    i = 0;
+                }
+            }
+
+            if (i != 0) {
+                stk.push(i);
+            }
+        }
+
+        vector<int> result(stk.size());
+        for (int i = stk.size() - 1; i >= 0; --i) {
+            result[i] = stk.top();
+            stk.pop();
+        }
+
+        return result;
+    }
+};
